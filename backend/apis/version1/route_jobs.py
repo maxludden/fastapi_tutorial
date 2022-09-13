77a2ab4f4fbe9c92401c9d4d4f9e0074b99c0db4
Max Ludden <dev@maxludden.com>
2022-09-13 12:37:11 -0400

    read_jobs.py

---
+++ backend/apis/version1/route_jobs.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from fastapi import Depends,HTTPException,status
+
+from db.session import get_db
+from db.models.jobs import Job
+from schemas.jobs import JobCreate,ShowJob
+from db.repository.jobs import create_new_job, retrieve_job# type: ignore
+
+router = APIRouter()
+
+
+@router.post("/create-job/",response_model=ShowJob)
+def create_job(job: JobCreate,db: Session = Depends(get_db)): # type: ignore
+    current_user = 1
+    job = create_new_job(job=job,db=db,owner_id=current_user) # type: ignore
+    return job
+
+
+#new function
+@router.get("/get/{id}",response_model=ShowJob) # if we keep just "{id}" . it would stat catching all routes
+def read_job(id:int,db:Session = Depends(get_db)):
+    job = retrieve_job(id=id,db=db) # type: ignore
+    if not job:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Job with this id {id} does not exist")
+    return job
